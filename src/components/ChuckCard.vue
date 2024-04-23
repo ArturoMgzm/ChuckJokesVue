8e2b6f970acb1a93596b6c634a82c238a96e9157
Arturo Manrique <arturomgzm@hotmail.com>
2024-04-22 19:09:01 -0600

    first commit

---
+++ src/components/ChuckCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card mb-3">
+      <img :src="iconUrl" class="card-img-top" alt="Chuck Norris">
+      <div class="card-body">
+        <p class="card-text">{{ value }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ChuckCard',
+    props: ['iconUrl', 'value']
+  }
+  </script>
+  
+  <style scoped>
+  .card {
+    border: 1px solid #FFFFFF;
+    border-radius: 0.25rem;
+    margin-bottom: 25px;
+  }
+  
+  .card-img-top {
+    height: 200px;
+    object-fit: cover;
+    border-top-left-radius: calc(0.25rem - 1px);
+    border-top-right-radius: calc(0.25rem - 1px);
+  }
+  
+  .card-body {
+    padding: 1rem;
+  }
+  
+  .card-text {
+    margin: 0;
+  }
+  </style>
